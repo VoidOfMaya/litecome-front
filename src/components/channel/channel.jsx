4ba3c15ec336269930538e68d84e8acc504ae201
Maya <MayaOfTheVoid@gmail.com>
2026-04-20 11:36:53 +0300

    creates basic message sending interface element and layout

--- src/components/channel/channel.jsx
+++ src/components/channel/channel.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import style from './channel.module.css'
+import sendMsg from '../../assets/icons/send.svg'
 const Channel = () =>{
     const [channels, setChannels] = useState(null);
     const [chnlMsgs, setChnlMsgs] = useState(<div>no chat open!</div>)
@@ -7,7 +8,14 @@ const Channel = () =>{
         <main className={style.channel}>
             <div className={style.sideNav}>Channel goes here</div>
             <div className={style.chatDisplay}> such emptieness,select a chat and start chattering</div>
-            <div className={style.chatInterface}>chat submit</div>
+            <div className={style.chatInterface}>
+                <button for='message' className={`${style.msgButton} ${style.rightBtn}`}>+</button>
+                <textarea id='message' name='message' className={style.msgTxtArea} placeholder='message @Group'>
+                </textarea>
+                <button for='message' className={`${style.msgButton} ${style.leftBtn}`}>
+                    <img src={sendMsg}></img>
+                </button>
+            </div>
         </main>
         
     )
