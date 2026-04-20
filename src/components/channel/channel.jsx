5a283b183a012ec102af3c8a3e7fdd9859a9c919
Maya <MayaOfTheVoid@gmail.com>
2026-04-20 10:43:38 +0300

    sets basic chat channels layout

--- src/components/channel/channel.jsx
+++ src/components/channel/channel.jsx
@@ -1,9 +1,15 @@
+import { useState } from 'react'
 import style from './channel.module.css'
 const Channel = () =>{
+    const [channels, setChannels] = useState(null);
+    const [chnlMsgs, setChnlMsgs] = useState(<div>no chat open!</div>)
     return(
         <main className={style.channel}>
-            <div>Channel goes here</div>
+            <div className={style.sideNav}>Channel goes here</div>
+            <div className={style.chatDisplay}> such emptieness,select a chat and start chattering</div>
+            <div className={style.chatInterface}>chat submit</div>
         </main>
+        
     )
 }
 export{
