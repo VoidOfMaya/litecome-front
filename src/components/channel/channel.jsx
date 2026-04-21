b30b2d96d1696eed2cb8d470d20ca0d4eb976bf9
Maya <MayaOfTheVoid@gmail.com>
2026-04-21 11:40:03 +0300

    creates message card component, requires refactoring to a sub-component

--- src/components/channel/channel.jsx
+++ src/components/channel/channel.jsx
@@ -1,13 +1,45 @@
-import { useState } from 'react'
-import style from './channel.module.css'
+import { useEffect, useState } from 'react';
+import style from './channel.module.css';
 import { ChatInterface } from './chatInterface/chatinterface';
+import {messages} from '../../mock/data'; // is a mock file simulating backend data
 const Channel = () =>{
     const [channels, setChannels] = useState(null);
-    const [chnlMsgs, setChnlMsgs] = useState(<div>no chat open!</div>)
+    const [chnlMsgs, setChnlMsgs] = useState(null);
+
+    /*maps messages to message component*/
+    const displayMsgs=(msgs)=>{
+        return msgs.map( msg=>{
+            return msg.reply_to? (
+                    <div key={msg.id} className={style.msgCardReply}>
+                        <div className={style.replyMsg}>reply to {msg.reply_to}</div>
+                        <div className={style.msgSuthor}>@{msg.author_id}</div>
+                        <div className={style.msgDate}>{msg.created_at}</div>
+                        <div className={style.msgTxt}>{msg.content}</div>
+                    </div>
+                ):(
+                    <div key={msg.id} className={style.msgCard}>
+                        <div className={style.msgSuthor}>@{msg.author_id}</div>
+                        <div className={style.msgDate}>{msg.created_at}</div>
+                        <div className={style.msgTxt}>{msg.content}</div> 
+                    </div> 
+                )
+                           
+        })
+    }
+    /*get data*/
+    useEffect(()=>{
+        /* simulating server data with a mock data file*/
+        setChnlMsgs(messages)
+        
+    },[])
     return(
         <main className={style.channel}>
             <div className={style.sideNav}>Channel goes here</div>
-            <div className={style.chatDisplay}> such emptieness,select a chat and start chattering</div>
+            <div className={style.chatDisplay}> 
+                {chnlMsgs? (
+                    displayMsgs(chnlMsgs)
+                ):('no chat open!')}
+                </div>
             <ChatInterface />
         </main>
         
